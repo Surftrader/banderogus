45e1651a8e85cf9cd419a9266e84fd8a852e9154
Sergiy Poddubka <poseal@gmail.com>
2024-09-16 16:25:51 +0300

    fix bugs

--- js/form.js
+++ js/form.js
@@ -1,12 +1,10 @@
-const form = document.querySelector('#form');
-const launchBtn = document.querySelector('#launch-btn');
-const goToFormButton = document.querySelector('#go-to-form-btn');
-const userEmailField = document.querySelector('#user-email');
-const userNameField = document.querySelector('#user-name');
+const form = document.querySelector("#form");
+const launchBtn = document.querySelector("#launch-btn");
+const goToFormButton = document.querySelector("#go-to-form-btn");
+const userEmailField = document.querySelector("#user-email");
+const userNameField = document.querySelector("#user-name");
 
-
-
-goToFormButton.addEventListener('click', function (e) {
+goToFormButton.addEventListener("click", function (e) {
     e.preventDefault();
     form.scrollIntoView();
 });
@@ -15,48 +13,46 @@ function clearFormFields() {
     const fieldName = form.querySelector('input[type="text"]');
     const fieldEmail = form.querySelector('input[type="email"]');
 
-    fieldName.value = '';
-    fieldEmail.value = '';
+    fieldName.value = "";
+    fieldEmail.value = "";
 }
 
 function addGooseElement() {
-    const targetContainer = document.querySelector('#form');
-    const gooseEl = document.createElement('img');
-    gooseEl.classList.add('gus-anim');
+    const targetContainer = document.querySelector("#form");
+    const gooseEl = document.createElement("img");
+    gooseEl.classList.add("gus-anim");
 
     targetContainer.appendChild(gooseEl);
 }
 
 function showGooseAnim() {
-    const gooseEl = document.querySelector('.gus-anim');
+    const gooseEl = document.querySelector(".gus-anim");
 
-    gooseEl.setAttribute('src', './img/gus-anim.gif');
-    
+    gooseEl.setAttribute("src", "./img/gus-anim.gif");
 
     setTimeout(() => {
-        gooseEl.removeAttribute('src');
-    }, 4000)
+        gooseEl.removeAttribute("src");
+    }, 4000);
 }
 
 addGooseElement();
 
-form.addEventListener('submit', e => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(form);
 
-    if (userEmailField?.value?.length > 30) {
-        return;
-    }
+    console.log("Імʼя користувача: ", userNameField.value);
+    console.log("Email користувача: ", userEmailField.value);
 
-    console.log('Імʼя користувача: ', userEmailField.value);
-    console.log('Email користувача: ', userNameField.value);
-
-    launchBtn.setAttribute('disabled', true);
-    launchBtn.style.opacity = '0.7';
+    launchBtn.setAttribute("disabled", true);
+    launchBtn.style.opacity = "0.7";
 
     showGooseAnim();
 
+    launchBtn.removeAttribute("disabled");
+    clearFormFields();
+
     setTimeout(() => {
-        launchBtn.style.opacity = '1';
-    }, 4000)
-})
+        launchBtn.style.opacity = "1";
+    }, 4000);
+});
